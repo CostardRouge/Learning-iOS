dfbddefebeba700eef07352bc9a2356ebf511bfc
CostardRouge <mncorp971@gmail.com>
2015-11-02 09:55:50 +0100

    init: standford assignements added

---
+++ Calculator/Calculator/StandfordViewController.swift
@@ -0,0 +1,87 @@
+//
+//  StandfordViewController.swift
+//  TumblrPlus
+//
+//  Created by Steeve Pommier on 21/10/15.
+//  Copyright © 2015 CostardRouge. All rights reserved.
+//
+
+import UIKit
+
+class StandfordViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    var brain = CalculatorBrain()
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+          //  userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        print("digit \(digit)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            }
+            else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        }
+        else {
+            displayValue = 0
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Steeve Pommier on 26/10/15.
+//  Copyright © 2015 CostardRouge. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+            //faceView.
+        }
+    }
+    
+    var happiness: Int = 100 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 2
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = Int(translation.y / Constants.HappinessGestureScale)
+            if (happinessChange != 0) {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
+
+    func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+}
